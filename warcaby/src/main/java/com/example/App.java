732664eb4883298d1bb44a7992c6db90cc74ea0d
Wojciech21 <wojciech.pindor.01@gmail.com>
2022-12-14 22:45:01 -0800

    GUI + server

--- warcaby/src/main/java/com/example/App.java
+++ warcaby/src/main/java/com/example/App.java
@@ -9,21 +9,51 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class App extends Application implements Runnable
+{
+  //temp
+  public Text t;
+   //TODO: posprzatac, zrobic klasy
+   //TODO: wysylanie info
+   //TODO: synchronizacja
 
     private static int WIDTH = 800;
     private static int HEIGHT = 600;
     private static int FIELDS = 10;
     private static int SIZE = Math.min(WIDTH, HEIGHT)/FIELDS;
     
-    private static int PLAYER = 1; //white
+
+    Socket socket = null;
+    PrintWriter out = null;
+    BufferedReader in = null;
+
+
+    private int player;
+
+    public final static int PLAYER1 = 1;
+    public final static int PLAYER2 = 2;
+
+    public final static int ACTIVE = 0;
+    public final static int NONACTIVE = 1;
+    private  static int actualPlayer = PLAYER1;
+
+    private static int showing = ACTIVE;
+
+
     /* (non-Javadoc)
      * @see javafx.application.Application#start(javafx.stage.Stage)
      */
@@ -111,14 +141,152 @@ public class App extends Application {
         }
       }
 
+      //temp
+      t = new Text(10, 50, "This is a test");
+      t.setFont(new Font(20));
+
+      gridPane.add(t,10,0);
       stage.setScene(new Scene(gridPane, WIDTH, HEIGHT));
       stage.show();
+      
+      
 
+
+        this.listenSocket();
+        this.receiveInitFromServer();
+        this.startThread();
+    }
+    
+
+    private void startThread() {
+          Thread gTh = new Thread(this);
+          gTh.start();
+      }
+  
+      @Override
+      public void run() {
+          if (player==PLAYER1) {
+              f1();
+          }
+          else{
+              f2();
+          }
+          // Mozna zrobic w jednej metodzie. Zostawiam
+          // dla potrzeb prezentacji
+          // f(player);
+      }
+
+          /// Metoda uruchamiana w run dla PLAYER1
+    void f1(){
+        while(true) {
+            synchronized (this) {
+                if (actualPlayer== PLAYER1) {
+                    try {
+                        wait(10);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                if (showing ==ACTIVE){
+                    receive();
+                    showing =NONACTIVE;
+                }
+                notifyAll();
+            }
+        }
     }
 
-
-    public static void main(String[] args) {
-        launch();
+    /// Metoda uruchamiana w run dla PLAYER2
+    void f2(){
+        while(true) {
+            synchronized (this) {
+                if (actualPlayer== PLAYER2) {
+                    try {
+                        wait(10);
+                    } catch (InterruptedException e) {
+                    }
+                }
+                if (showing ==ACTIVE){
+                    receive();
+                    showing =NONACTIVE;
+                }
+                notifyAll();
+            }
+        }
     }
+
+    private void send(String text)
+    {
+        // Wysylanie do serwera
+      out.println("test");
+        // msg.setText("OppositeTurn");
+        // send.setEnabled(false);
+        // input.setText("");
+        // input.requestFocus();
+        // showing = ACTIVE;
+        // actualPlayer = player;
+    }
+    private void receive(){
+      try {
+          // Odbieranie z serwera
+          String str = in.readLine();
+          t.setText(str);
+      }
+      catch (IOException e) {
+          System.out.println("Read failed"); System.exit(1);}
+  }
+  public void listenSocket() {
+    try {
+        socket = new Socket("localhost", 4444);
+        // Inicjalizacja wysylania do serwera
+        out = new PrintWriter(socket.getOutputStream(), true);
+        // Inicjalizacja odbierania z serwera
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    } catch (UnknownHostException e) {
+        System.out.println("Unknown host: localhost");
+        System.exit(1);
+    } catch (IOException e) {
+        System.out.println("No I/O");
+        System.exit(1);
+    }
+}
+  private void receiveInitFromServer() {
+      try {
+          player = Integer.parseInt(in.readLine());
+          if (player == PLAYER1) {
+              t.setText("My Turn");
+          } else {
+              t.setText("Opposite turn");
+          }
+      } catch (IOException e) {
+          System.out.println("Read failed");
+          System.exit(1);
+      }
+  }
+
+//     // Jedna metoda dla kazdego Playera
+//     void f(int iPlayer){
+//         while(true) {
+//             synchronized (this) {
+//                 if (actualPlayer== iPlayer) {
+//                     try {
+//                         wait(10);
+//                     } catch (InterruptedException e) {
+//                     }
+//                 }
+//                 if (showing ==ACTIVE){
+//                     receive();
+//                     showing =NONACTIVE;
+//                 }
+//                 notifyAll();
+//             }
+//         }
+//     }
+
+
+
+    
+public static void main(String[] args) {
+  launch();
+}
 
 }
--- warcaby/src/main/java/com/example/Server.java
+++ warcaby/src/main/java/com/example/Server.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.example;
 
 import java.io.*;
 import java.net.*;
