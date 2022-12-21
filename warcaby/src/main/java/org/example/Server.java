55f1a9518aca8981400103af3bfd103730ab720b
Wojciech21 <wojciech.pindor.01@gmail.com>
2022-12-21 12:03:58 -0800

    package fix

---
+++ warcaby/src/main/java/org/example/Server.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    static BufferedReader in;
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket(4444)) {
+
+            System.out.println("Server is listening on port 4444");
+
+            while (true) 
+            {
+                Socket firstClient = serverSocket.accept();
+                System.out.println("First client connected");
+                System.out.println("Waiting for the second player");
+
+                Socket secondClient = serverSocket.accept();
+                System.out.println("Second client connected");
+
+                Game g = new Game(firstClient, secondClient);
+                Thread gTh = new Thread(g);
+                gTh.start();
+
+
+
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
