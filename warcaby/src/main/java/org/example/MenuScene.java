38b3866b436beffbf03ea9d7d2c6c96014679e21
Mikołaj <mikolajdsp10@gmail.com>
2023-01-12 21:31:33 +0100

    poprawa launchera

--- warcaby/src/main/java/org/example/MenuScene.java
+++ warcaby/src/main/java/org/example/MenuScene.java
@@ -1,15 +1,21 @@
 package org.example;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+
+import java.net.URISyntaxException;
 
 public class MenuScene extends SceneParent implements Runnable
 {
@@ -35,6 +41,7 @@ public class MenuScene extends SceneParent implements Runnable
   public void waitingForPlayer()
   {
     waitLabel.setText("Waiting for another player");
+    waitLabel.setFont(Font.font("Garamond",37));
     game1Button.setDisable(true);
     game2Button.setDisable(true);
     game3Button.setDisable(true);
@@ -42,94 +49,59 @@ public class MenuScene extends SceneParent implements Runnable
 
   public void run()
   {
-    vbox = new VBox(HEIGHT/10);
+    vbox = new VBox(HEIGHT/40);
     vbox.setAlignment(Pos.CENTER);
-    
+    vbox.setBackground(new Background(new BackgroundFill(Color.SIENNA, CornerRadii.EMPTY, Insets.EMPTY)));
     scene = new Scene(vbox, WIDTH, HEIGHT);
-    
     waitLabel = new Label("");
-    waitLabel.setPrefHeight(HEIGHT/10);
+    waitLabel.setPrefHeight(HEIGHT/11);
     waitLabel.setPrefWidth(WIDTH);
     waitLabel.setAlignment(Pos.CENTER);
     waitLabel.setFont(new Font(HEIGHT/15));
-    vbox.getChildren().add(waitLabel);
 
-    game1Button = new Button("Game1");
-    game1Button.setPrefHeight(HEIGHT/6);
+    Image image;
+    try {
+      image = new Image(getClass().getResource("logoszachy2.png").toURI().toString());
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
+    ImageView imageView = new ImageView(image);
+    imageView.autosize();
+    imageView.setPreserveRatio(true);
+    imageView.setFitHeight(200);
+    vbox.getChildren().add(imageView);
+    vbox.getChildren().add(waitLabel);
+    game1Button = new Button("Polish Checkers");
+    game1Button.setPrefHeight(HEIGHT/7);
     game1Button.setPrefWidth(WIDTH/2);
+
+    game1Button.setStyle("-fx-background-color: beige");
+    game1Button.setFont(Font.font("Garamond", 30));
     
-    game1Button.setOnMouseEntered(new EventHandler<MouseEvent>() 
-    {
-      public void handle(MouseEvent me) {
-        scene.setCursor(Cursor.HAND);
-      }
-    });
-    game1Button.setOnMouseExited(new EventHandler<MouseEvent>() 
-    {
-      public void handle(MouseEvent me) 
-      {
-        scene.setCursor(Cursor.CROSSHAIR);
-      }
-    });
-    game1Button.setOnAction(new EventHandler<ActionEvent>() 
-    {
-        public void handle(ActionEvent event)
-        {
-          app.startGame("GAME1");
-        }
-    });
+    game1Button.setOnMouseEntered(me -> scene.setCursor(Cursor.HAND));
+    game1Button.setOnMouseExited(me -> scene.setCursor(Cursor.CROSSHAIR));
+    game1Button.setOnAction(event -> app.startGame("GAME1"));
     vbox.getChildren().add(game1Button);
 
-    game2Button = new Button("Game2");
-    game2Button.setPrefHeight(HEIGHT/6);
+    game2Button = new Button("English Checkers");
+    game2Button.setPrefHeight(HEIGHT/7);
     game2Button.setPrefWidth(WIDTH/2);
-    
-    game2Button.setOnMouseEntered(new EventHandler<MouseEvent>() 
-    {
-      public void handle(MouseEvent me) {
-          scene.setCursor(Cursor.HAND);
-      }
-    });
-    game2Button.setOnMouseExited(new EventHandler<MouseEvent>() 
-    {
-      public void handle(MouseEvent me) 
-      {
-          scene.setCursor(Cursor.CROSSHAIR);
-      }
-    });
-    game2Button.setOnAction(new EventHandler<ActionEvent>() 
-    {
-        public void handle(ActionEvent event)
-        {
-          app.startGame("GAME2");
-        }
-    });
+    game2Button.setStyle("-fx-background-color: beige");
+    game2Button.setFont(Font.font("Garamond",30));
+    game2Button.setOnMouseEntered(me -> scene.setCursor(Cursor.HAND));
+    game2Button.setOnMouseExited(me -> scene.setCursor(Cursor.CROSSHAIR));
+    game2Button.setOnAction(event -> app.startGame("GAME2"));
     vbox.getChildren().add(game2Button);
 
-    game3Button = new Button("Game3");
-    game3Button.setPrefHeight(HEIGHT/6);
+    game3Button = new Button("Thai Checkers");
+    game3Button.setPrefHeight(HEIGHT/7);
     game3Button.setPrefWidth(WIDTH/2);
+    game3Button.setStyle("-fx-background-color: beige");
+    game3Button.setFont(Font.font("Garamond",30));
     
-    game3Button.setOnMouseEntered(new EventHandler<MouseEvent>() 
-    {
-      public void handle(MouseEvent me) {
-          scene.setCursor(Cursor.HAND);
-      }
-    });
-    game3Button.setOnMouseExited(new EventHandler<MouseEvent>() 
-    {
-      public void handle(MouseEvent me) 
-      {
-          scene.setCursor(Cursor.CROSSHAIR);
-      }
-    });
-    game3Button.setOnAction(new EventHandler<ActionEvent>() 
-    {
-        public void handle(ActionEvent event)
-        {
-          app.startGame("GAME3");
-        }
-    });
+    game3Button.setOnMouseEntered(me -> scene.setCursor(Cursor.HAND));
+    game3Button.setOnMouseExited(me -> scene.setCursor(Cursor.CROSSHAIR));
+    game3Button.setOnAction(event -> app.startGame("GAME3"));
     vbox.getChildren().add(game3Button);
   }
 }
